e7a7e7f34e4149329cb23844b2490135394d1c91
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-09-19 22:58:15 +0200

    finish footer nav component including design

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,28 +1,37 @@
 import "./Navigation.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouseUser,
+  faBookmark,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
 export default function Navigation() {
   return (
     <>
       <nav className="App-Navigation">
-        <ul class="header__navbar__liste">
-          <li class="header__navbar__liste__items">
-            <a href="index.html" class="header__navbar__liste__items__link">
-              <div class="header__navbar__liste__items__link--HomeIsActive">
-                <i class="fa-solid fa-house-user"></i>
-              </div>
+        <ul className="App-Navigation-Nav">
+          <li className="App-Navigation-Nav-Li">
+            <a href="#" className="App-Navigation-Nav-Li-A">
+              <FontAwesomeIcon
+                className="App-Navigation-Nav-Li-A-Icon"
+                icon={faHouseUser}
+              />
             </a>
           </li>
-          <li class="header__navbar__liste__items">
-            <a href="bookmark.html" class="header__navbar__liste__items__link">
-              <div class="header__navbar__liste__items__link__div">
-                <i class="fa-solid fa-bookmark"></i>
-              </div>
+          <li className="App-Navigation-Nav-Li">
+            <a href="#" className="App-Navigation-Nav-Li-A">
+              <FontAwesomeIcon
+                className="App-Navigation-Nav-Li-A-Icon"
+                icon={faBookmark}
+              />
             </a>
           </li>
-          <li class="header__navbar__liste__items">
-            <a href="user.html" class="header__navbar__liste__items__link">
-              <div class="header__navbar__liste__items__link__div">
-                <i class="fa-solid fa-user"></i>
-              </div>
+          <li className="App-Navigation-Nav-Li">
+            <a href="#" className="App-Navigation-Nav-Li-A">
+              <FontAwesomeIcon
+                className="App-Navigation-Nav-Li-A-Icon"
+                icon={faUser}
+              />
             </a>
           </li>
         </ul>
--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
+    <script
+      defer
+      src="https://kit.fontawesome.com/1ea256a700.js"
+      crossorigin="anonymous"
+    ></script>
   </React.StrictMode>
 );
 
