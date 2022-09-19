1a5baa8d98c3f1494105caa1844f04bf80215e88
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-09-19 19:37:17 +0200

    add basic structure including components and finish Header Component

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+import "./Navigation.css";
+export default function Navigation() {
+  return (
+    <section className="App-Navigation">
+      <div>Home</div>
+      <div>Bookmark</div>
+      <div>User</div>
+    </section>
+  );
+}
---
+++ src/components/header/Header.js
@@ -0,0 +1,10 @@
+import SvgComponent from "../Assets/Logo.js";
+import "./Header.css";
+export default function Header() {
+  return (
+    <section className="Header">
+      <SvgComponent className="Header-logo"></SvgComponent>
+      <h1 className="Header-heading1">{"Now with React"}</h1>
+    </section>
+  );
+}
