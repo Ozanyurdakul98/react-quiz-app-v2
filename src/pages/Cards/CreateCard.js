c5915201331240074b7a5e264246a7cf15ada282
Ozan Leon Yurdakul <ozan.yurdakul58@gmail.com>
2022-09-26 23:00:15 +0200

    fix createCard preview; form input displayed in preview

--- src/pages/Cards/CreateCard.js
+++ src/pages/Cards/CreateCard.js
@@ -6,8 +6,11 @@ import "./CreateCard.css";
 export default function CreateCard({ newCard }) {
   const [question, setQuestion] = useState("");
   //To change
-  const [answer = "Your answer goes here", setAnswer] = useState();
-  const [tag, setTag] = useState();
+  const [answer, setAnswer] = useState("");
+  const [tag, setTag] = useState("");
+  const [tag2, setTag2] = useState("");
+  const [tag3, setTag3] = useState("");
+  const [tag4, setTag4] = useState("");
   //strings verwenden
 
   function createForm(event) {
@@ -35,11 +38,32 @@ export default function CreateCard({ newCard }) {
           <div className="App-main-Card-Button">
             <button className="App-main-Card-Button-Button">Show Answer</button>
             <div className="App-main-Card-Button-QuizAnswer--active">
-              <p>{answer}</p>
+              <p>{answer === "" ? "Your answer goes here" : answer}</p>
             </div>
           </div>
           <div className="App-main-Card-Tags">
-            <ul className="App-main-Card-Tags-UL"></ul>
+            <ul className="App-main-Card-Tags-UL">
+              <li>
+                <a href="/#" className={"App-main-Card-Tags-UL-A "}>
+                  {tag === "" ? "TAG#1" : tag}
+                </a>
+              </li>{" "}
+              <li>
+                <a href="/#" className={"App-main-Card-Tags-UL-A "}>
+                  {tag2 === "" ? "TAG#2" : tag2}
+                </a>
+              </li>{" "}
+              <li>
+                <a href="/#" className={"App-main-Card-Tags-UL-A "}>
+                  {tag3 === "" ? "TAG#3" : tag3}
+                </a>
+              </li>{" "}
+              <li>
+                <a href="/#" className={"App-main-Card-Tags-UL-A "}>
+                  {tag4 === "" ? "TAG#4" : tag4}
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
         <div className="body-s__main-s__createQuiz-s__createQuiz-c__form">
@@ -87,6 +111,10 @@ export default function CreateCard({ newCard }) {
                   <input
                     type="text"
                     name="newTag"
+                    value={tag}
+                    onChange={(event) => {
+                      setTag(event.target.value);
+                    }}
                     id="newTag"
                     minLength="2"
                     maxLength="15"
@@ -100,6 +128,10 @@ export default function CreateCard({ newCard }) {
                   <input
                     type="text"
                     name="newTag2"
+                    value={tag2}
+                    onChange={(event) => {
+                      setTag2(event.target.value);
+                    }}
                     id="newTag2"
                     minLength="2"
                     maxLength="15"
@@ -113,6 +145,10 @@ export default function CreateCard({ newCard }) {
                   <input
                     type="text"
                     name="newTag3"
+                    value={tag3}
+                    onChange={(event) => {
+                      setTag3(event.target.value);
+                    }}
                     id="newTag3"
                     minLength="2"
                     maxLength="15"
@@ -126,6 +162,10 @@ export default function CreateCard({ newCard }) {
                   <input
                     type="text"
                     name="newTag4"
+                    value={tag4}
+                    onChange={(event) => {
+                      setTag4(event.target.value);
+                    }}
                     id="newTag4"
                     minLength="2"
                     maxLength="15"
